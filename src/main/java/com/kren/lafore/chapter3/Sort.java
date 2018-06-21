8711a4a705fadd3efe692a1bb4699dc23287b456
krenMax <krenmv@belhard.com>
2018-06-21 10:30:28 +0300

    reading book

--- src/main/java/com/kren/lafore/chapter3/Sort.java
+++ src/main/java/com/kren/lafore/chapter3/Sort.java
@@ -9,7 +9,47 @@ public class Sort {
     public static int[] bubbleSort(int[] array) {
 
 	int[] sortedArray = array.clone();
+	int lastIndex = sortedArray.length - 1;
+
+	for (int i = lastIndex; i >= 0; i--) {
+	    for (int j = 0; j < i; j++) {
+		int current = sortedArray[j];
+		int next = sortedArray[j + 1];
+
+		if (current > next) {
+		    switchElements(sortedArray, j, j + 1);
+		}
+	    }
+	}
 
 	return sortedArray;
+    }
+
+    public static int[] selectSort(int[] array) {
+
+	int[] sortedArray = array.clone();
+
+	for (int i = 0; i < sortedArray.length; i++) {
+	    int minIndex = i;
+
+	    for (int j = i; j < sortedArray.length; j++) {
+		if (sortedArray[minIndex] > sortedArray[j]) {
+		    minIndex = j;
+		}
+	    }
+
+	    if (minIndex != i) {
+		switchElements(sortedArray, minIndex, i);
+	    }
+
+	}
+
+	return sortedArray;
+    }
+
+    private static void switchElements(int[] array, int firstIndex, int secondIndex) {
+	int firstValue = array[firstIndex];
+	array[firstIndex] = array[secondIndex];
+	array[secondIndex] = firstValue;
     }
 }
--- src/test/java/com/kren/lafore/chapter3/SortTest.java
+++ src/test/java/com/kren/lafore/chapter3/SortTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public class SortTest {
 
-    private static final int[] array = { 2, 5, 8, 9, 6, 7 };
+    private static final int[] array = { 7, 9, 8, 5, 6, 2 };
 
     private static final int[] sortedArray = array.clone();
 
@@ -21,4 +21,8 @@ public class SortTest {
 	assertArrayEquals(Sort.bubbleSort(array), sortedArray);
     }
 
+    @Test
+    void selectSort() {
+	assertArrayEquals(Sort.selectSort(array), sortedArray);
+    }
 }
